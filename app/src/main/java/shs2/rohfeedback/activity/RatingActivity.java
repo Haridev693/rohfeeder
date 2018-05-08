dc6b8a1e799cbe4f2ecb042e50a2dace641ca6b4
haridevnirgude <haridevnirgude@gmail.com>
2018-05-08 09:51:53 +0530

    Version 1

---
+++ app/src/main/java/shs2/rohfeedback/activity/RatingActivity.java
@@ -0,0 +1,15 @@
+package shs2.rohfeedback.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import shs2.rohfeedback.R;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
---
+++ app/src/main/java/shs2/rohfeedback/utility/GlobalVariable.java
@@ -0,0 +1,19 @@
+package shs2.rohfeedback.utility;
+
+import android.app.Application;
+
+import shs2.rohfeedback.object.RestAddress;
+
+//import com.shss.restaurantwaiter.object.RestAddress;
+
+/**
+ * Created by abc on 3/12/2018.
+ */
+
+public class GlobalVariable extends Application {
+
+    public static int taxAmount=0;
+    public static RestAddress restAddress;
+    public static int custid;
+    public static String details;
+}
